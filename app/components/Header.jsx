1da382064ba37f7bd266ba0bfd61c497a791bcc6
i-corag <icoraglio@gmail.com>
2023-08-27 12:32:53 +0200

    modify header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { MdLocalPhone, MdMail } from 'react-icons/md'
 import DropdownNavbar from "./DropdownNavbar"
 import Navbar from "./Navbar"
@@ -5,7 +6,18 @@ import Navbar from "./Navbar"
 
 
 const Header = () => {
-    const icons = ['/public/icon_instagram.svg', '/public/icon_facebook.svg']
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        const landing = document.getElementById('landing');
+        const offset = 112;
+        const topPosition = landing.offsetTop;
+        window.scrollTo({
+            top: topPosition - offset,
+            behavior: 'smooth',
+        });
+    }
+
     return (
         <header className='w-screen sticky top-0 z-10 flex flex-col'>
 
@@ -19,7 +31,9 @@ const Header = () => {
             </div>
 
             <div className="h-20 md:h-28 py-2 px-6 flex justify-between items-center bg-white font-museo lg:px-8 md:text-white md:bg-black">
-                <a href='#landing'
+                <a
+                    href='#landing'
+                    onClick={(event) => handleClick(event)}
                     className="h-full w-1/4 md:w-2/12 bg-[url(/logo_reduccion.png)] sm:bg-[url(/logo.png)] bg-contain bg-no-repeat bg-start md:bg-center" />
 
                 <div className="h-full w-1/4 md:w-9/12 flex items-center justify-center md:pb-4 lg:pb-3">
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,9 @@
 'use client'
-
 import { useState } from "react";
 
 const Navbar = () => {
 
     const [activeSection, setActiveSection] = useState('');
-
 
     const handleNavClick = (event, targetId) => {
         event.preventDefault();
@@ -22,35 +20,36 @@ const Navbar = () => {
     };
 
     return (
-        <nav className='hidden w-full h-full md:flex flex-row justify-center gap-4 items-end md:text-xl lg:text-3xl xl:text-4xl xl:gap-8'>
+        <nav className='hidden w-full h-full md:flex flex-row justify-center gap-4 items-end md:text-xl lg:text-3xl xl:text-4xl xl:gap-8 '>
             <a
-                href='#nosotros'
+                href='nosotros'
                 onClick={(event) => handleNavClick(event, 'nosotros')}
-                className={activeSection === 'nosotros' ? 'underline' : ''}
-            >Nosotros
+                className={activeSection === 'nosotros' ? 'underline' : ''}>
+                Nosotros
             </a>
             <a
-                href='#productos'
+                href='productos'
                 onClick={(event) => handleNavClick(event, 'productos')}
                 className={activeSection === 'productos' ? 'underline' : ''}
             >
                 Productos
             </a>
             <a
-                href='#sustentabilidad'
+                href='sustentabilidad'
                 onClick={(event) => handleNavClick(event, 'sustentabilidad')}
                 className={activeSection === 'sustentabilidad' ? 'underline' : ''}
             >
                 Sustentabilidad
             </a>
             <a
-                href='#contacto'
+                href='contacto'
                 onClick={(event) => handleNavClick(event, 'contacto')}
                 className={activeSection === 'contacto' ? 'underline' : ''}
             >
                 Contacto
             </a>
         </nav>
+
     )
 }
 
