e5787224b9fcf93026388a0bfb930b43a795c4a1
i-corag <icoraglio@gmail.com>
2023-09-19 20:44:50 +0200

    nosotros

--- components/Nosotros.jsx
+++ components/Nosotros.jsx
@@ -1,14 +1,15 @@
+import Image from "next/image"
 
 const Nosotros = () => {
     return (
         <>
             <h1 className='flex justify-center pt-4 font-museo text-4xl md:hidden'>Nosotros</h1>
-            <div className='flex flex-col mx-auto w-[90%] sm:w-[80%] md:w-[95%] h-[calc(100vh-112px)] md:flex-row md:items-center md:justify-center md:gap-6 md:py-8'>
-                <div className="w-full md:w-1/2 h-[50%] md:h-full xl:h-[80%] flex justify-center items-center my-4 rounded-xl bg-fepGreen">
-                    Imagen Fabrica
+            <div className='flex flex-col mx-auto w-full sm:w-[90%] md:w-[95%] h-[calc(100vh-112px)] md:flex-row md:items-center md:justify-center md:gap-6'>
+                <div className="w-full md:w-1/2 h-[30%] my-2 md:h-[80%] relative">
+                    <Image alt='nosotros' src='/nosotros.png' fill sizes='h-full' className="object-cover sm:rounded-xl" />
                 </div>
 
-                <p className='text-center font-museo text-sm sm:text-base md:w-1/2 md:text-xl md:text-center xl:text-2xl'>
+                <p className='text-center font-museo text-sm w-[90%] mx-auto sm:text-base md:w-1/2 md:text-center xl:text-2xl'>
                     Ubicada en la pintoresca ciudad costera de Mar del Plata, Buenos Aires, nuestra empresa se
                     enorgullece de ser líder en la fabricación y distribución de envases plásticos. Con años de
                     experiencia en la industria, ofrecemos soluciones innovadoras y sostenibles para satisfacer las
