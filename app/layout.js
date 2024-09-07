ecf063ac5fb87cba0f6dd2a3ccc2509ae4473a05
gianfranco <gguardamagna23@gmail.com>
2024-09-07 15:04:59 +0200

    debug GTM

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import localFont from 'next/font/local';
 import Footer from '../components/Footer';
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script';
 
 const museo = localFont({
   src: '../public/fonts/Museo.ttf',
@@ -21,12 +21,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='es'>
+      <head>
+      <Script
+      id="gtm"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-WB4T4V2C');`
+      }}
+      />
+      </head>
       <body className={`${museo.variable}  ${tusker.variable}`}>
         {children}
         <Footer />
       </body>
-      <GoogleTagManager gtmId='GTM-WB4T4V2C'/>
     </html>
   );
 }
